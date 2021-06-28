0877ec225fa5c9098869d52efbf9b49dc55caaf5
SergioAndrade22 <sergio.david2201@gmail.com>
2021-06-28 13:46:15 -0300

    Moved information to .env file

--- src/services/FamilySizeAPI.ts
+++ src/services/FamilySizeAPI.ts
@@ -1,18 +1,18 @@
 import axios from 'axios'
 
 export default class FamilySizeAPI {
-  public async getMenu (): Promise<any> {
-    const res = await axios.get('https://familysize-servicio-web.herokuapp.com/menu')
+  public async getMenu (): Promise<[]> {
+    const res = await axios.get(`${process.env.familySizeURL}/menu`)
     return res.data
   }
 
-  public async getMenuItem (id: number): Promise<any> {
-    const res = await axios.get(`https://familysize-servicio-web.herokuapp.com/menu/${id}`)
+  public async getMenuItem (id: number): Promise<[]> {
+    const res = await axios.get(`${process.env.familySizeURL}/menu/${id}`)
     return res.data
   }
 
-  public async getMenuItemPicture (id: number): Promise<any> {
-    const res = await axios.get(`https://familysize-servicio-web.herokuapp.com/menu/${id}/picture`)
+  public async getMenuItemPicture (id: number): Promise<[]> {
+    const res = await axios.get(`${process.env.familySizeURL}/menu/${id}/picture`)
     return res.data
   }
 }
--- src/services/FixerIOAPI.ts
+++ src/services/FixerIOAPI.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export default class FixerIOAPI {
-  public async getCurrencies (): Promise<any> {
-    const res = await axios.get('http://data.fixer.io/api/latest?access_key=e5302df52b328e44c952c456e3c406eb&symbols=EUR,USD,AUD,GBP,CAD,PLN')
+  public async getCurrencies (): Promise<[]> {
+    const res = await axios.get(`http://data.fixer.io/api/latest?access_key=${process.env.fixerAPIKey}&symbols=EUR,USD,AUD,GBP,CAD,PLN`)
     return res.data
   }
 }
